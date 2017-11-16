b1179d92729322fe972118c05149d09d52014916
Michel Akira <michel.akira@gmail.com>
2017-11-16 10:29:48 -0200

    Categorias VS Produtos

--- admin-categories.php
+++ admin-categories.php
@@ -3,21 +3,9 @@
 use Oeste\PageAdmin;
 use Oeste\Model\User;
 use Oeste\Model\Category;
+use Oeste\Model\Product;
 
-$app->get("/categories/:idcategory", function ($idcategory){
 
-    $category = new Category();
-    
-    $category->get((int)$idcategory);
-    
-    $page = new Page();
-    
-    $page->setTpl("category",[
-        'category'=>$category->getValues(),
-        'products'=>[]
-    ]);
-    
-});
 
 $app->get("/admin/categories", function (){
 
@@ -102,6 +90,62 @@ $app->post("/admin/categories/:idcategory", function ($idcategory){
     $category->save();
     
     header('Location: /admin/categories');
+    exit;
+    
+});
+
+$app->get("/admin/categories/:idcategory/products", function ($idcategory){
+
+    User::verifyLogin();
+    
+    $category = new Category();
+    
+    $category->get((int)$idcategory);
+    
+    $page = new PageAdmin();
+    
+    $page->setTpl("categories-products",[
+        'category'=>$category->getValues(),
+        'productsRelated'=>$category->getProducts(),
+        'productsNotRelated'=>$category->getProducts(false)
+    ]);
+    
+});
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/add", function ($idcategory, $idproduct){
+
+    User::verifyLogin();
+    
+    $category = new Category();
+    
+    $category->get((int)$idcategory);
+    
+    $product = new Product();
+    
+    $product->get((int)$idproduct);
+    
+    $category->addProduct($product);
+
+    header("Location: /admin/categories/".$idcategory."/products");
+    exit;
+    
+});
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/remove", function ($idcategory, $idproduct){
+
+    User::verifyLogin();
+    
+    $category = new Category();
+    
+    $category->get((int)$idcategory);
+    
+    $product = new Product();
+    
+    $product->get((int)$idproduct);
+    
+    $category->removeProduct($product);
+
+    header("Location: /admin/categories/".$idcategory."/products");
     exit;
     
 });
