cee78681c033f653bd85d8ea3f1e7e0d2dfcf367
Michel Akira <michel.akira@gmail.com>
2017-11-21 11:36:39 -0200

    WEB service endereço

--- functions.php
+++ functions.php
@@ -1,25 +1,50 @@
 <?php 
 
 use Oeste\Model\User;
+use Oeste\Model\Cart;
 
 function formatPrice($vlprice)
 {
-    if(!$vlprice > 0) $vlprice = 0;
-        
-    return number_format($vlprice, 2, ",", ".");
+
+	if (!$vlprice > 0) $vlprice = 0;
+
+	return number_format($vlprice, 2, ',', '.');	
+	
 }
 
-function  checkLogin($inadmin){
-    
-    return User::checkLogin($inadmin);
-    
+function formatDate($date)
+{
+	return date('d/m/Y', strtotime($date));
 }
 
-function getUserName(){
-    
-    $user = User::getFromSession();
-    
-    return $user->getdesperson();
+function checkLogin($inadmin = true)
+{
+	return User::checkLogin($inadmin);
+}
+
+function getUserName()
+{
+	$user = User::getFromSession();
+
+	return $user->getdesperson();
+}
+
+function getCartNrQtd()
+{
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+}
+
+function getCartVlSubTotal()
+{
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
 }
 
  ?>
